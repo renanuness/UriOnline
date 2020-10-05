caae853976bf2096c886cf86824a0ad022dce128
renanuness <renan1504@gmail.com>
2020-10-04 23:26:24 -0300

    ex2058

---
+++ adhoc/csharp/ex2058/ex2058.cs
@@ -0,0 +1,11 @@
+using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        var lados = Int32.Parse(Console.ReadLine());
+
+        Console.Write("{0}\n", lados - 2);
+    }
+}
