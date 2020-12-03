c318cc62f5a3f3553496b8656553374605fc7289
renanuness <renan1504@gmail.com>
2020-12-03 10:12:16 -0200

    1278 funcionando

---
+++ adhoc/csharp/ExerciciosTDD/src/ExerciciosApp/ex1278/Ex1278.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ExerciciosApp.ex1278
+{
+    public static class URI
+    {
+        static void Main(string[] args)
+        {
+            var ex = new Ex1278();
+            List<List<string>> respostas = new List<List<string>>();
+
+            var casosDeTeste = ex.LerCasosDeTeste();
+            do
+            {
+                var texto = ex.LerTexto(casosDeTeste);
+                texto = ex.RemoverEspacos(texto);
+                var maiorLinha = ex.CalcularMaiorLinha(texto);
+                texto = ex.AlinharDireita(texto, maiorLinha);
+
+                respostas.Add(texto);
+
+                casosDeTeste = ex.LerCasosDeTeste();
+            } while (casosDeTeste != 0);
+
+
+            for(int i = 0; i < respostas.Count; i++)
+            {
+                respostas[i].ForEach( t => Console.Write("{0}\n", t));
+                if(i < respostas.Count -1)
+                    Console.Write("\n");
+            }
+        }
+
+
+
+        public static string RemoverEspacos(this string texto)
+        {
+            texto = texto.Trim();
+
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex("[ ]{2,}", options);
+            texto = regex.Replace(texto, " ");
+            return texto;
+        }
+
+    }
+
+    public class Ex1278
+    {
+        private string LerLinha()
+        {
+            return Console.ReadLine();
+        }
+
+        public List<string> AlinharDireita(List<string> texto, int offSet)
+        {
+            for (int i = 0; i < texto.Count; i++)
+                texto[i] = texto[i].PadLeft(offSet);
+
+            return texto;
+        }
+
+        public List<string> RemoverEspacos(List<string> texto)
+        {
+            for (int i = 0; i < texto.Count; i++)
+                texto[i] = texto[i].RemoverEspacos();
+
+            return texto;
+        }
+
+        public int LerCasosDeTeste()
+        {
+            return int.Parse(LerLinha());
+        }
+
+        public List<string> LerTexto(int linhas)
+        {
+            List<string> texto = new List<string>();
+            for (int i = 0; i < linhas; i++)
+                texto.Add(LerLinha());
+
+            return texto;
+        }
+
+        public int CalcularMaiorLinha(List<string> texto)
+        {
+            return texto.OrderByDescending(t => t.Length).FirstOrDefault().Length;
+        }
+    }
+}
---
+++ adhoc/csharp/ExerciciosTDD/tests/Exercicios.Tests/ex1278Tests.cs
@@ -0,0 +1,37 @@
+﻿using ExerciciosApp.ex1278;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Exercicios.Tests
+{
+    public class ex1278Tests
+    {
+        [Fact(DisplayName = "Remover Espacos Duplicados")]
+        [Trait("Categoria", "Ex 1278")]
+        public void LinhasComVariosEspacos_DeveRetirarEspacosDuplicados()
+        {
+            // Arrange
+            var ex = new Ex1278();
+
+            List<string> linhas = new List<string>();
+            var linha1 = "    teste  teste      teste      ";
+            var linha2 = "  teste    teste   teste         ";
+            var linha3 = "   teste  teste     teste    ";
+            linhas.Add(linha1);
+            linhas.Add(linha2);
+            linhas.Add(linha3);
+
+            // Act 
+            var result = ex.RemoverEspacos(linhas);
+        
+            // Assert
+            foreach(var linha in result)
+            {
+                var contains = linha.Contains("  ");
+                Assert.False(contains);
+            }
+        }
+    }
+}
