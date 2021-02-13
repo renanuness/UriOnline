c6773c6a60a42b05c21ea29c92d0fd6a8af86519
renanuness <renan1504@gmail.com>
2021-02-13 13:56:24 -0200

    1868

---
+++ adhoc/csharp/ExerciciosTDD/src/ExerciciosStrings/ex1868/Ex1868.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace ExerciciosStrings.Exercicio1868
+{
+    public class URI
+    {
+        static void Main(string[] args)
+        {
+            var ex = new Ex1868();
+            ex.Executar();
+        }
+    }
+
+    public class Ex1868
+    {
+        StringBuilder linhaComO;
+
+        public void Executar()
+        {
+            var n = 0;
+            while ((n = int.Parse(Console.ReadLine())) != 0)
+            {
+                CriarMatriz(n);
+            }
+        }
+
+        private void CriarMatriz(int n)
+        {
+            int[,] matriz = new int[n, n];
+
+            var coordenada = new Coordenada(n / 2, n / 2);
+
+            List<Direcao> direcoes = new List<Direcao>();
+            direcoes.Add(new Direita());
+            direcoes.Add(new Cima());
+            direcoes.Add(new Esquerda());
+            direcoes.Add(new Baixo());
+
+            var direcao = 0;
+
+            var tamanhoDoPasso = 1;
+            var passos = 0;
+            var vezesPasso = 0;
+            var limite = n * n;
+            linhaComO = new StringBuilder().Append('O', n);
+
+            for (int i = 0; i < limite; i++)
+            {
+                ImprimirMatriz(n, coordenada.X, coordenada.Y);
+
+                if (passos < tamanhoDoPasso)
+                {
+                    var d = direcao % 4;
+                    coordenada = direcoes[d].Andar(coordenada.X, coordenada.Y);
+
+                    passos++;
+                    if (passos == tamanhoDoPasso)
+                    {
+                        direcao++;
+                        passos = 0;
+                        vezesPasso++;
+                        if (vezesPasso == 2)
+                        {
+                            tamanhoDoPasso++;
+                            vezesPasso = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void ImprimirMatriz(int n, int x, int y)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (i == x)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (x != i || y != j)
+                            Console.Write("O");
+                        else
+                            Console.Write("X");
+                    }
+                }
+                else
+                {
+                    Console.Write("{0}", linhaComO);
+                }
+
+                Console.Write("\n");
+            }
+            Console.Write("@\n");
+        }
+    }
+    public struct Coordenada
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coordenada(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public abstract class Direcao
+    {
+        public virtual Coordenada Andar(int i, int j)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Direita : Direcao
+    {
+        public override Coordenada Andar(int i, int j)
+        {
+            return new Coordenada(i, ++j);
+        }
+    }
+
+    public class Cima : Direcao
+    {
+        public override Coordenada Andar(int i, int j)
+        {
+            return new Coordenada(--i, j);
+        }
+    }
+
+    public class Esquerda : Direcao
+    {
+        public override Coordenada Andar(int i, int j)
+        {
+            return new Coordenada(i, --j);
+        }
+    }
+
+    public class Baixo : Direcao
+    {
+        public override Coordenada Andar(int i, int j)
+        {
+            return new Coordenada(++i, j);
+        }
+    }
+}
---
+++ adhoc/csharp/ExerciciosTDD/src/ExerciciosStrings/ex2242/Ex2242.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ExerciciosStrings.Exercicio2242
+{
+    public class URI
+    {
+        static void Main(string[] args)
+        {
+            var ex = new Ex2242();
+            ex.Executar();
+        }
+    }
+
+    public class Ex2242
+    {
+        public void Executar()
+        {
+            var risada = Console.ReadLine();
+
+            var vogais = ExtrairConsoantes(risada);
+            CompararVogais(vogais);
+        }
+
+        private void CompararVogais(string vogais)
+        {
+            var sb = new StringBuilder();
+            for(int i = vogais.Length-1; i >= 0; i--)
+                sb.Append(vogais[i]);
+
+            if (vogais == sb.ToString())
+                Console.Write("S\n");
+            else
+                Console.Write("N\n");
+        }
+
+        private string ExtrairConsoantes(string risada)
+        {
+            char[] vogais = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            var sb = new StringBuilder();
+            for(int i = 0; i < risada.Length; i++)
+            {
+                var l = risada[i];
+                if (vogais.Contains(l))
+                    sb.Append(l);
+            }
+            return sb.ToString();
+        }
+    }
+}
